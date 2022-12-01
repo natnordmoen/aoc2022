2dcb509fc8d4d5d6fd91c89a7773506726530b8a
Natalia Nordmoen <natalia.nordmoen@gjensidige.no>
2022-12-01 12:20:07 +0100

    refactored

--- day1.py
+++ day1.py
@@ -1,26 +1,22 @@
 #!/usr/bin/env python
 
-def day1():
+def solution2():
     f = open("day1_input", "r")
     calories = 0
-    max_calories = 0
     all_calories = []
     for line in f.readlines():
         if line not in ['\n', '\r\n']:
             calories += int(line)
         else:
             all_calories.append(calories)
-            if calories > max_calories:
-                max_calories = calories
-                calories = 0
-            else:
-                calories = 0
-    return max_calories, all_calories
+            calories = 0
+    return all_calories
 
+def day1():
+    all_calories = solution2()
+    sum_top_three = 0
+    [sum_top_three := sum_top_three + x for x in sorted(all_calories, reverse=True)[:3]]
+    print("Part 1: ", sorted(all_calories, reverse=True)[0])
+    print("Part 2: ", sum_top_three)
 
-max_calories, all_calories = day1() 
-sum_top_three = 0
-[sum_top_three := sum_top_three + x for x in sorted(all_calories, reverse=True)[:3]]
-
-print("Part 1: ", max_calories)
-print("Part 2: ", sum_top_three)
+day1()
